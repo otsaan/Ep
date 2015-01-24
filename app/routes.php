3fe00730793d59de9c611fc6e058c6d6733434f3
zianwar <anwaar.ziani@gmail.com>
2015-01-24 14:08:31 +0000

    Add signup page and route

--- app/routes.php
+++ app/routes.php
@@ -20,3 +20,8 @@ Route::get('/login', function()
 {
     return View::make('login');
 });
+
+Route::get('/signup', function()
+{
+    return View::make('signup');
+});
---
+++ app/views/signup.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html class="bg-black">
+<head>
+    <meta charset="UTF-8">
+    <title>Ep | Signup</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    {{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css') }}
+    {{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css') }}
+    <!-- Ionicons -->
+    {{ HTML::style('//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') }}
+    <!-- Theme style -->
+    {{ HTML::style('css/style.css') }}
+</head>
+<body class="bg-black">
+
+<div class="form-box" id="login-box">
+    <div class="header">Sign up</div>
+    <form action="../../index.html" method="post">
+        <div class="body bg-gray">
+            <div class="form-group">
+                <input type="text" name="name" class="form-control" placeholder="Full name"/>
+            </div>
+            <div class="form-group">
+                <input type="text" name="userid" class="form-control" placeholder="User ID"/>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" class="form-control" placeholder="Password"/>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password2" class="form-control" placeholder="Retype password"/>
+            </div>
+        </div>
+        <div class="footer">
+
+            <button type="submit" class="btn bg-olive btn-block">Sign me up</button>
+
+            <a href="login.html" class="text-center">I already have an account</a>
+        </div>
+    </form>
+</div>
+
+{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js') }}
+{{ HTML::script('//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js') }}
+
+</body>
+</html>
