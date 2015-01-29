687c92a6fa2ab41215e82a752b7000530187ea9d
otmgue <the_loverboy@hotmail.fr>
2015-01-29 14:44:14 +0000

    Add validation rules

--- app/Ep/Forms/RegistrationForm.php
+++ app/Ep/Forms/RegistrationForm.php
@@ -10,9 +10,13 @@ class RegistrationForm extends FormValidator {
      * validation rules for registration form
      */
     protected $rules = [
+        'first_name'=>'required',
+        'last_name'=>'required',
         'username' => 'required|unique:users',
+        'is_type' => 'in:etudiant,professeur,laureat',
         'email' => 'required|email|unique:users',
-        'password' => 'required|confirmed'
+        'password' => 'required|min:6|confirmed'
+
     ];
 }
 
