94857a0baa193028519ef66fa0b1f70e1608b066
zianwar <anwaar.ziani@gmail.com>
2015-01-29 03:12:17 +0000

    Quick edit

--- app/routes.php
+++ app/routes.php
@@ -1,37 +1,16 @@
 <?php
 use Ep\Factory\UserFactory;
 
+App::bind('Laracasts\Commander\CommandTranslator','Laracasts\Commander\BasicCommandTranslator');
+Route::get('/store','PostController@store');
+Event::listen('Ep.Posts.PostWasPublished','EmailNotifier');
+
+
+
 Route::get('/', [
     'as' => 'home',
     'uses' => "pagesController@home"
 ]);
-/*function()
-{
-
-    --------- UserFactory Tests---------
-        run 'composoer dump-autoload' to load UserFactory class
-
-    UserFactory::createStudent([
-        'first_name' => 'student',
-        'last_name' => 'student',
-        'cne' => '11762398'
-    ]);
-
-    UserFactory::createGraduate([
-        'first_name' => 'graduate',
-        'last_name' => 'graduate',
-        'graduation_year' => '2000',
-        'job' => 'CEO'
-    ]);
-
-    UserFactory::createProfessor([
-        'first_name' => 'Professor',
-        'last_name' => 'Professor'
-    ]);
-
-    -------------------------------------
-return View::make('index');
-});*/
 
 
 Route::get('/login', function () {
@@ -45,3 +24,37 @@ Route::get('/signup', [
 Route::post('/signup', [
 
 ]);
+
+Route::get('/home', function()
+{
+
+    /*
+        --------- UserFactory Tests---------
+            run 'composoer dump-autoload' to load UserFactory class
+
+        UserFactory::createStudent([
+            'first_name' => 'student',
+            'last_name' => 'student',
+            'cne' => '11762398'
+        ]);
+
+        UserFactory::createGraduate([
+            'first_name' => 'graduate',
+            'last_name' => 'graduate',
+            'graduation_year' => '2000',
+            'job' => 'CEO'
+        ]);
+
+        UserFactory::createProfessor([
+            'first_name' => 'Professor',
+            'last_name' => 'Professor'
+        ]);
+
+        -------------------------------------
+    */
+    $posts = Channel::findOrFail(4)->posts;
+
+
+    return View::make('channels.feed', compact('posts'));
+});
+
