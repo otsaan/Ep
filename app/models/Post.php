4be53f1aa23e028e9d3f33d0bc38d58eae3ec285
zianwar <anwaar.ziani@gmail.com>
2015-01-29 03:03:11 +0000

    Implement PostPresenter

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,6 +1,15 @@
 <?php
 
-class Post extends \Eloquent {
+use Laracasts\Commander\Events\EventGenerator;
+use Laracasts\Presenter\Contracts\PresentableInterface;
+use Laracasts\Presenter\PresentableTrait;
+
+class Post extends \Eloquent implements  PresentableInterface {
+
+    use EventGenerator;
+    use PresentableTrait;
+
+    protected $presenter = 'Ep\Posts\PostPresenter';
 
     protected $guarded = array('id');
     protected $hidded = array('created_at','updated_at');
@@ -23,5 +32,17 @@ class Post extends \Eloquent {
     public function attachments()
     {
         return $this->morphMany('Attachment','attachable');
+    }
+
+    public function publish($content)
+    {
+        dd($content);
+        $this->content = $content;
+        //$this->save();
+        dd("post saved");
+        $event = new PostWasPublished($content);
+        $this->raise($event);
+
+        return $this;
     }
 }
