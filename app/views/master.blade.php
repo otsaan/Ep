2ae1fe0f3c2058fb787c433f0b6353d0bb67d94f
zianwar <anwaar.ziani@gmail.com>
2015-01-30 19:17:01 +0000

    Tiny fixes

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -31,7 +31,7 @@
         <!-- header logo: style can be found in header.less -->
         <header class="header">
 
-            <a href="index.html" class="logo">
+            <a href="/feed" class="logo">
                 <img src="img/small_logo.png">
             </a>
 
