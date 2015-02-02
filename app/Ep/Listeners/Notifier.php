c87832d4639c98c05cd8f663c52a386fb40394ae
otmgue <the_loverboy@hotmail.fr>
2015-02-02 00:11:04 +0000

    Add comments notifications

--- app/Ep/Listeners/Notifier.php
+++ app/Ep/Listeners/Notifier.php
@@ -2,6 +2,7 @@
 
 use Laracasts\Commander\Events\EventListener;
 use Ep\Posts\PostWasPublished;
+use Ep\Comments\CommentWasPublished;
 use Fenos\Notifynder\Facades\Notifynder;
 
 class Notifier extends EventListener
@@ -27,8 +28,24 @@ class Notifier extends EventListener
         Notifynder::sendMultiple($MultiNotificationsArray);
     }
 
-    //TODO
-    /*
-     *      * whenCommentWasPublished
-     */
+    public function   whenCommentWasPublished(CommentWasPublished $event){
+        $toUserId = $event->comment->post->user->id;
+
+        $postChannel = $event->comment->post->channel;
+
+        $post = $channel = $event->comment->post;
+        //user
+        if ($toUserId != \Auth::user()->id) {
+
+            $notificationData = Notifynder::builder()->from('User', \Auth::user()->id)
+                ->to('User', $toUserId)
+                ->category('comment')
+                ->url("http://localhost:8000/channels/{$postChannel->id}/posts/{$post->id}")
+                ->extra($postChannel->name)
+                ->getArray();
+
+            Notifynder::send($notificationData);
+        }
+    }
+
 }
--- app/views/components/navbar.blade.php
+++ app/views/components/navbar.blade.php
@@ -77,36 +77,7 @@
     </ul>
 </li>
 <!-- Notifications: style can be found in dropdown.less -->
-<li class="dropdown notifications-menu">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="ion ion-ios-information-outline"></i>
-        @if($notifications->count()>0)
-        <span class="label bg-blue">{{$notifications->count()}}</span>
-        @else
-        <span class="label bg-dark-gray">0</span>
-        @endif
-    </a>
-    <ul class="dropdown-menu">
-        <li class="header">You have {{$notifications->count()}} notification(s)</li>
-
-        <li>
-            <!-- inner menu: contains the actual data -->
-            <ul class="menu">
-                @foreach($notifications as $notification)
-                <li>
-                    <a href="{{ $notification['url'] }}?notifId={{ $notification['id'] }}">
-                        <i class="fa fa-users warning"></i>
-                        <b> {{$notification['from']['first_name']}} {{$notification['from']['last_name']}}</b>
-                        {{$notification['body']['text']}} {{$notification['extra']}} at <b> {{date("d-M-Y",
-                            strtotime($notification['created_at']))}}</b>
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-        </li>
-        <li class="footer"><a href="{{route('read_notifications')}}">Mark as read</a></li>
-    </ul>
-</li>
+@include('components.partials.notifications')
 <!-- Tasks: style can be found in dropdown.less -->
 <li class="dropdown tasks-menu">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
