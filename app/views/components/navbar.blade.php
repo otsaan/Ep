8b164da7a5a112fc0582d50197e06c0185763edc
otmgue <the_loverboy@hotmail.fr>
2015-02-10 01:56:09 +0000

    Cleaning navebare

--- app/views/components/navbar.blade.php
+++ app/views/components/navbar.blade.php
@@ -8,84 +8,7 @@
 <!-- Messages: style can be found in dropdown.less-->
 <!-- Notifications: style can be found in dropdown.less -->
 @include('components.partials.notifications')
-<!-- Tasks: style can be found in dropdown.less -->
-<li class="dropdown tasks-menu">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="ion ion-ios-list-outline"></i>
-        <span class="label bg-dark-gray">9</span>
-    </a>
-    <ul class="dropdown-menu">
-        <li class="header">You have 9 tasks</li>
-        <li>
-            <!-- inner menu: contains the actual data -->
-            <ul class="menu">
-                <li><!-- Task item -->
-                    <a href="#">
-                        <h3>
-                            Design some buttons
-                            <small class="pull-right">20%</small>
-                        </h3>
-                        <div class="progress xs">
-                            <div class="progress-bar progress-bar-aqua" style="width: 20%" role="progressbar"
-                                 aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                <span class="sr-only">20% Complete</span>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <!-- end task item -->
-                <li><!-- Task item -->
-                    <a href="#">
-                        <h3>
-                            Create a nice theme
-                            <small class="pull-right">40%</small>
-                        </h3>
-                        <div class="progress xs">
-                            <div class="progress-bar progress-bar-green" style="width: 40%" role="progressbar"
-                                 aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                <span class="sr-only">40% Complete</span>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <!-- end task item -->
-                <li><!-- Task item -->
-                    <a href="#">
-                        <h3>
-                            Some task I need to do
-                            <small class="pull-right">60%</small>
-                        </h3>
-                        <div class="progress xs">
-                            <div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar"
-                                 aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                <span class="sr-only">60% Complete</span>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <!-- end task item -->
-                <li><!-- Task item -->
-                    <a href="#">
-                        <h3>
-                            Make beautiful transitions
-                            <small class="pull-right">80%</small>
-                        </h3>
-                        <div class="progress xs">
-                            <div class="progress-bar progress-bar-yellow" style="width: 80%" role="progressbar"
-                                 aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                <span class="sr-only">80% Complete</span>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <!-- end task item -->
-            </ul>
-        </li>
-        <li class="footer">
-            <a href="#">View all tasks</a>
-        </li>
-    </ul>
-</li>
+
 <!-- User Account: style can be found in dropdown.less -->
 <li class="dropdown user user-menu">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -95,24 +18,10 @@
     <ul class="dropdown-menu">
         <!-- User image -->
         <li class="user-header bg-light-blue">
-            <img src="{{ isset($user->photo) ? asset($user->photo) : asset('img/avatar5.png') }}" class="img-circle" alt="User Image"/>
-            <p>
-                {{$user->first_name." ".$user->last_name}} - Web Developer
-                <small>Member since Nov. 2012</small>
-            </p>
+            <img style="width: 150px;height: 150px;" src="{{ isset($user->photo) ? asset($user->photo) : asset('img/avatar5.png') }}" class="img-circle" alt="User Image"/>
         </li>
         <!-- Menu Body -->
-        <!-- <li class="user-body">
-            <div class="col-xs-4 text-center">
-                <a href="#">Followers</a>
-            </div>
-            <div class="col-xs-4 text-center">
-                <a href="#">Sales</a>
-            </div>
-            <div class="col-xs-4 text-center">
-                <a href="#">Friends</a>
-            </div>
-        </li> -->
+
         <!-- Menu Footer-->
         <li class="user-footer">
             <div class="pull-left">
