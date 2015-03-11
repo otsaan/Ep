c5cad290b44835c4192907967e8a71f532e6dfe3
zianwar <anwaar.ziani@gmail.com>
2015-03-11 02:03:04 +0000

    Remove conflict comments

--- app/views/admin/posts.blade.php
+++ app/views/admin/posts.blade.php
@@ -44,27 +44,7 @@
     @stop
 
     @section('right')
-<<<<<<< HEAD
         @include('components.adminbar')
-=======
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">Administration</h3>
-        </div>
-        <div class="panel-body">
-            <ul class="list-group">
-                <li class="list-group-item"><a href="/admin/users"><div style="width:100%" class="btn btn-primary">Administration des utilisateurs</div></a></li>
-                <li class="list-group-item"><a href="/admin/groups"><div style="width:100%" class="btn btn-primary">Administration des groupes</div></a></li>
-                <li class="list-group-item"><a href="/admin/posts"><div style="width:100%" class="btn btn-primary">Administration des posts</div></a></li>
-                <li class="list-group-item"><a href="/admin/posts"><div style="width:100%" class="btn btn-primary">Statistiques</div></a></li>
-            </ul>
-
-
-
-        </div>
-    </div>
-
->>>>>>> origin/master
     @stop
 
 
